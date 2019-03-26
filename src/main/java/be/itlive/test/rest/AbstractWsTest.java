a08016ca13be81f52f447db9e30c18e122600a91
biertvi <biertvi@D02DI1419995COM.net1.cec.eu.int>
2019-03-26 09:12:16 +0100

    pattern change

--- src/main/java/be/itlive/test/rest/AbstractWsTest.java
+++ src/main/java/be/itlive/test/rest/AbstractWsTest.java
@@ -7,29 +7,31 @@ import be.itlive.test.logging.AbstractTestLogger;
 
 /**
  * Allow a integration test to work locally but also on Bamboo.
+ * 
  * @author vbiertho
  */
 public abstract class AbstractWsTest extends AbstractTestLogger {
-    protected static final String LOCAL_WORKSTATION_PATERN = "(LE|TE)\\d{7}";
+	protected static final String LOCAL_WORKSTATION_PATERN = "D02\\d{10}";
 
-    /**
-     * Should return the good domain.
-     * @return a domain (ex : humanresourcetest , servicestest...)
-     */
-    public abstract String getTestDomain();
+	/**
+	 * Should return the good domain.
+	 * 
+	 * @return a domain (ex : integration , acceptance...)
+	 */
+	public abstract String getTestDomain();
 
-    /**
-     * @return localHost or the test domain.
-     * @throws UnknownHostException if unknow host.
-     */
-    public String getDomain() throws UnknownHostException {
-        try {
-            if (InetAddress.getLocalHost().getHostName().matches(LOCAL_WORKSTATION_PATERN)) {
-                return "localhost:8080";
-            }
-        } catch (final UnknownHostException e) {
-            throw e;
-        }
-        return getTestDomain();
-    }
+	/**
+	 * @return localHost or the test domain.
+	 * @throws UnknownHostException if unknow host.
+	 */
+	public String getDomain() throws UnknownHostException {
+		try {
+			if (InetAddress.getLocalHost().getHostName().matches(LOCAL_WORKSTATION_PATERN)) {
+				return "localhost:8080";
+			}
+		} catch (final UnknownHostException e) {
+			throw e;
+		}
+		return getTestDomain();
+	}
 }
